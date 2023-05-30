20199aad8a63da220717fab78bec6cf6db57b99a
Diogo Machado <119526075+Murdock021@users.noreply.github.com>
2023-05-30 16:51:00 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -23,9 +23,17 @@ const { saveApiCountry } = require("./src/controllers/saveApiCountry.js");
 const { PORT } = process.env;
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(async() => {
+conn.sync({ force: false}).then(() => {
+  server.listen(3001, () => {
+    console.log('Server listening '); // eslint-disable-line no-console
+  });
+});
+
+
+/* conn.sync({ force: true }).then(async() => {
   await saveApiCountry()
   server.listen(PORT, () => {
     console.log('%s listening at ', PORT); // eslint-disable-line no-console
   });
 });
+ */
